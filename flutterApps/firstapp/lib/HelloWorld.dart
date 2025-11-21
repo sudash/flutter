885a56b50543cfb6816f4b7efba032791cd65ce7
sudash <sunnydayalshaw@gmail.com>
2025-11-22 05:26:31 +0530

    Update HelloWorld.dart
    Understood how custom Widgets can be created. 
    Moved the container widget and all its child components into a custom Widget.

--- flutterApps/firstapp/lib/HelloWorld.dart
+++ flutterApps/firstapp/lib/HelloWorld.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print('first mobile app ');
+  print('first mobile app using flutter');
 
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 100, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello Flutter World!',
-              style: TextStyle(color: Colors.white, fontSize: 30,),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 100, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          'Hello Flutter World From Custom Widget!',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+    );
+  }
 }
